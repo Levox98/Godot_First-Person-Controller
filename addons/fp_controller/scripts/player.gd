a1348e9f3fa8896ba966460dbb84cd304cd8fab6
Kirill Ivanov <ivanov.ks101@gmail.com>
2024-09-28 14:24:40 +0500

    fixed camera motion with controllers

--- addons/fp_controller/scripts/player.gd
+++ addons/fp_controller/scripts/player.gd
@@ -23,6 +23,7 @@ var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @export var regular_climb_speed: float = 6.0
 @export var fast_climb_speed: float = 8.0
 @export_range(0.0, 1.0) var view_bobbing_amount: float
+@export_range(1, 100) var camera_sensitivity: int
 
 @export_group("Feature toggles")
 @export var allow_jump: bool = true
@@ -126,9 +127,8 @@ func _physics_process(delta: float) -> void:
 	if can_move:
 		if Input.get_vector(MOVE_LEFT, MOVE_RIGHT, MOVE_FORWARD, MOVE_BACK):
 			input_direction = Input.get_vector(MOVE_LEFT, MOVE_RIGHT, MOVE_FORWARD, MOVE_BACK)
-		## TODO: fix controller input
-		#elif Input.get_connected_joypads().size() != 0:
-			#input_direction = Vector2(Input.get_joy_axis(0, JOY_AXIS_LEFT_X), Input.get_joy_axis(0, JOY_AXIS_LEFT_Y))
+		elif Input.get_connected_joypads().size() != 0:
+			input_direction = Vector2(Input.get_joy_axis(0, JOY_AXIS_LEFT_X), Input.get_joy_axis(0, JOY_AXIS_LEFT_Y))
 		else:
 			input_direction = Vector2.ZERO
 	
@@ -150,8 +150,8 @@ func _process(_delta: float):
 		# Handling camera in '_process' so that camera movement is framerate independent
 		_handle_camera_motion()
 	
-	#if Input.get_connected_joypads().size() != 0 && InputEventJoypadMotion:
-		#_handle_joy_camera_motion()
+	if Input.get_connected_joypads().size() != 0:
+		_handle_joy_camera_motion()
 
 
 func _handle_camera_motion() -> void:
@@ -165,25 +165,18 @@ func _handle_camera_motion() -> void:
 	mouse_motion = Vector2.ZERO
 
 ## TODO: fix camera movement with controller
-#func _handle_joy_camera_motion() -> void:
-	#var x_axis = Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)
-	#var y_axis = Input.get_joy_axis(0, JOY_AXIS_RIGHT_X)
-	#
-	#var current_x: float = 0
-	#var current_y: float = 0
-	#
-	#current_x = current_x - x_axis
-	#current_y = current_y - y_axis
-	#
-	#rotate_y(-y_axis)
-	#camera_pivot.rotate_x(-x_axis)
-	#
-	#camera_pivot.rotation_degrees.x = clampf(
-		#camera_pivot.rotation_degrees.x , -89.0, 89.0
-	#)
-	#
-	#current_x = 0
-	#current_y = 0
+func _handle_joy_camera_motion() -> void:
+	var x_axis = Input.get_joy_axis(0, JOY_AXIS_RIGHT_X)
+	var y_axis = Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)
+	var resulting_vector = Vector2(x_axis, y_axis)
+	var normalized_resulting_vector = resulting_vector.normalized()
+	
+	rotate_y(-deg_to_rad(camera_sensitivity * normalized_resulting_vector.x))
+	camera_pivot.rotate_x(-deg_to_rad(camera_sensitivity * normalized_resulting_vector.y))
+	
+	camera_pivot.rotation_degrees.x = clampf(
+		camera_pivot.rotation_degrees.x , -89.0, 89.0
+	)
 
 
 func check_climbable() -> bool:
